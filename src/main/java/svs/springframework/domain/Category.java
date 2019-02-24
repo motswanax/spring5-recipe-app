45c414d4f0eb99d5e07a85214f62567a6d49db64
Baiketli Dijo <baiketlid@gmail.com>
2019-02-24 17:27:17 +0000

    - added the project lombok dependency to the POM file. - auto refactored the Category class with @Data project lombok annotation. - added default data methods from @Data refactoring on Notes. - manually refactored the Recipe class with @Data from project lombok.

--- src/main/java/svs/springframework/domain/Category.java
+++ src/main/java/svs/springframework/domain/Category.java
@@ -1,5 +1,7 @@
 package svs.springframework.domain;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.util.Set;
 
@@ -7,6 +9,7 @@ import java.util.Set;
  * @author BAD
  * @version 24/02/19
  */
+@Data // project lombok refactoring
 @Entity
 public class Category {
 
@@ -18,27 +21,4 @@ public class Category {
     @ManyToMany(mappedBy = "categories")
     private Set<Recipe> recipes;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Set<Recipe> getRecipes() {
-        return recipes;
-    }
-
-    public void setRecipes(Set<Recipe> recipes) {
-        this.recipes = recipes;
-    }
 }
--- src/main/java/svs/springframework/services/RecipeServiceImpl.java
+++ src/main/java/svs/springframework/services/RecipeServiceImpl.java
@@ -1,5 +1,6 @@
 package svs.springframework.services;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import svs.springframework.domain.Recipe;
 import svs.springframework.repositories.RecipeRepository;
@@ -11,6 +12,7 @@ import java.util.Set;
  * @author BAD
  * @version 24/02/19
  */
+@Slf4j // project lombok logging
 @Service
 public class RecipeServiceImpl implements RecipeService {
     private final RecipeRepository recipeRepository;
@@ -21,6 +23,7 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("I'm in the service.");
         Set<Recipe> recipeSet = new HashSet<>();
 
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
