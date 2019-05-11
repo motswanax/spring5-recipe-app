59aae30ca47907763fd5001b047dd04d04337535
Baiketli Dijo <baiketlid@gmail.com>
2019-05-11 23:38:17 +0100

    adding global exception handling with Controller-Advice

---
+++ src/main/java/svs/springframework/controllers/ControllerExceptionHandler.java
@@ -0,0 +1,32 @@
+package svs.springframework.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author BAD
+ * @version 11/05/19
+ */
+@Slf4j
+@ControllerAdvice // handles exception on a global level
+public class ControllerExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(NumberFormatException.class)
+    public ModelAndView handleNumberFormat(Exception exception){
+
+        log.error("Handling Number Format Exception");
+        log.error(exception.getMessage());
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("400error");
+        modelAndView.addObject("exception", exception);
+
+        return modelAndView;
+    }
+}
